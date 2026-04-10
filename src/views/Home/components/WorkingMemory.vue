c1bf6ff6385ad21ceb7c1179f0e5fb0306ef1576
Tiantian Xue (iSoftStone Information) <v-tiantxue@microsoft.com>
2026-04-10 09:35:53 +0800

    feat: fuse文案修改

--- src/views/Home/components/WorkingMemory.vue
+++ src/views/Home/components/WorkingMemory.vue
@@ -518,7 +518,7 @@ const secondaryTopNavItems = computed(() => [
   },
   {
     key: "Fuse",
-    label: "Fuse",
+    label: "✨Fuse",
     hintKey: "Fuse",
     active: false,
     disabled: !canUseFuse.value,
